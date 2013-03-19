7e94d6652f5e0ec585a467105029d5e387afac82
Simon Lovely <s.lovely+git@gmail.com>
2013-03-19 19:50:27 +0000

    Added a service that will be used to test database access

---
+++ src/WcfExamples.Contracts/IDatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfExamples.Contracts
+{
+    [ServiceContract]
+    public interface IDatabaseService
+    {
+        [OperationContract]
+        Person GetObjectFromDatabase(int id);
+    }
+
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ src/WcfExamples.Services/DatabaseService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfExamples.Contracts;
+
+namespace WcfExamples.Services
+{
+    public class DatabaseService : IDatabaseService
+    {
+        public Person GetObjectFromDatabase(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
